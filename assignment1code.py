e6c1a1fe82d1875ddc60a9811a6e6c5a8c79aa62
austenzhang <austengzhang@gmail.com>
2024-05-09 09:50:01 -0600

    Completed assignment

--- assignment1code.py
+++ assignment1code.py
@@ -1,10 +1,11 @@
 # ENSF 692 Spring 2024
 # Assignment 1 Starter Code
-# Add your name here!
+# Author: Austen Zhang
 
-studentName = "Sarah Shah"  # Change the contents of this variable to your name (leave the quotation marks).
+studentName = "Austen Zhang"  # Change the contents of this variable to your name (leave the quotation marks).
 
 print("Hello, World!")
 print("My name is " + studentName + " and I am a student in ENSF 692.")
 
 # print your best skill here
+print("My best skill is drawing.")
